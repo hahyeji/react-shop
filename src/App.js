83039c092526814910bedcdf40d4fa7e71543ac7
하혜지 <hhjhhj1313@gmail.com>
2023-11-02 10:29:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { Button, Navbar } from "bootstrap";
+import { createContext, useState } from "react";
+import data from "./data.js";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import { MainNav, Main, About, Event } from "./MainContents";
+import Cart from "./routes/Cart";
+
+let Context1 = createContext();
 
 function App() {
+  let [shoes, setShoes] = useState(data);
+  let [moreCount, setMoreCount] = useState(0);
+  let [재고] = useState([10, 11, 12]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainNav />
+      <RoutesAll
+        shoes={shoes}
+        setShoes={setShoes}
+        moreCount={moreCount}
+        setMoreCount={setMoreCount}
+      />
     </div>
   );
 }
+
+const RoutesAll = ({ shoes, setShoes, moreCount, setMoreCount }) => {
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<div>없는 페이지입니다</div>} />
+        <Route
+          path="/"
+          element={
+            <Main
+              shoes={shoes}
+              setShoes={setShoes}
+              moreCount={moreCount}
+              setMoreCount={setMoreCount}
+            />
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>about > member</div>} />
+          <Route path="location" element={<div>about > location</div>} />
+        </Route>
+
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문 시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰 받기</div>} />
+        </Route>
+
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
